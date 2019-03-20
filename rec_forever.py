1e9aeec6e48d39b3ba244f77fbd98225af933d5f
Hirofumi Miura <ladiciushm@gmail.com>
2019-03-20 15:52:43 +0900

    Use ring buffer in recorder

--- rec_forever.py
+++ rec_forever.py
@@ -2,17 +2,18 @@ import asyncio
 import logging
 from queue import Queue
 from threading import Thread
-import time
-import wave
 
+import numpy as np
 import pyaudio
+
+from ring_buffer import ShortRingBuffer
 
 # ビット深度
 FORMAT = pyaudio.paInt16
 # 1チャンネル=モノラル
 CHANNELS = 1
 # サンプリングレート(Hz)
-RATE = 44100
+RATE = 16_000
 # 1フレームの秒数
 FRAME_SEC = 0.04
 # 1フレームのbufferサイズ
@@ -20,15 +21,19 @@ CHUNK = int(RATE * FRAME_SEC)
 
 logging.basicConfig(
     level=logging.DEBUG,
-    format='[%(levelname)s] (%(threadName)-10s) %(message)s',
+    format="[%(levelname)s](%(process)s-%(threadName)s) @%(asctime)s"
+           " - %(name)s.%(filename)s#%(funcName)s():L%(lineno)s"
+           " - %(message)s"
 )
 
 
 class Recorder:
     def __init__(self):
-        self.q = Queue()
+        self.buf_input_queue = Queue()
+        self.buf_output_queue = Queue()
         self._audio = pyaudio.PyAudio()
         self._stream = None
+        self._ring_buf = ShortRingBuffer(capacity=CHUNK * 20, frame_size=CHUNK*4)
 
     def start_rec(self):
         logging.debug('start_rec')
@@ -41,15 +46,22 @@ class Recorder:
             stream_callback=self.callback
         )
         logging.debug("recording...")
-        th = Thread(
-            name='Handler',
-            target=self.handle_buf,
-            args=(self.q,)
+        threads = (
+            Thread(
+                name='Buffering',
+                target=self.buffering,
+            ),
+            Thread(
+                name='ReadBufferd',
+                target=self.read_buffered,
+            ),
+            Thread(
+                name='HandleReadBuf',
+                target=self.handle_read_buf,
+            ),
         )
-        th.start()
-        th.join()
-        self._stream.close()
-        self._audio.terminate()
+        for th in threads:
+            th.start()
 
     def stop_rec(self):
         logging.debug('stop_rec')
@@ -64,17 +76,28 @@ class Recorder:
             status
     ):
         logging.debug(f'in_data len:{len(in_data)} type:{type(in_data)}')
-        self.q.put(in_data)
+        self.buf_input_queue.put(in_data)
         return None, pyaudio.paContinue
 
-    def handle_buf(self, q: Queue):
-        logging.debug(f'start handle_buf')
+    def buffering(self):
+        while True:
+            buf = self.buf_input_queue.get()
+            logging.debug(f'👇 write buf:{buf[:10]}({len(buf)}) <- from buf_input_queue')
+            self._ring_buf.write(buf)
 
+    def read_buffered(self):
+        while True:
+            while self._ring_buf.is_filled():
+                buf = self._ring_buf.read()
+                logging.debug(f'👆 read buf:{buf[:10]}({len(buf)}) -> put buf_output_queue')
+                self.buf_output_queue.put(buf)
+
+    def handle_read_buf(self):
         async def coro():
             while self._stream.is_active():
-                buf = q.get()
-                await asyncio.sleep(0.3)
-                logging.debug(f'dequeued buf:{len(buf)}')
+                buf = self.buf_output_queue.get()
+                logging.debug(f'🙌 fetched buffer buf:{buf[:10]}({len(buf)})')
+                #  int16arr = np.frombuffer(buf, dtype=np.int16)
 
         loop = asyncio.new_event_loop()
         asyncio.set_event_loop(loop)
